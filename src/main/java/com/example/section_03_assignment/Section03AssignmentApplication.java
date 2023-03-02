fe15cf060b551270a66f7c885a8f0dc58be97192
Zac Edens <edens.zac@gmail.com>
2023-03-01 20:33:17 -0800

    initial spring framework

---
+++ src/main/java/com/example/section_03_assignment/Section03AssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.section_03_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section03AssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section03AssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/section_03_assignment/Section03AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.section_03_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section03AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
